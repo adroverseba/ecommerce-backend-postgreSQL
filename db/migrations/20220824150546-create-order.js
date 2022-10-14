e64421bf672f03b8600007da06e55ef53fc3d92d
adroverseba <adroversebastian.sa@gmail.com>
2022-10-14 14:16:13 -0300

    fix migration order

--- db/migrations/20220824150546-create-order.js
+++ db/migrations/20220824150546-create-order.js
@@ -1,10 +1,35 @@
 'use strict';
 
-const { ORDER_TABLE, OrderSchema } = require('../models/order.model');
+const { ORDER_TABLE } = require('../models/order.model');
+const { CUSTOMER_TABLE } = require('../models/customer.model');
+const { Sequelize, DataTypes } = require('sequelize');
 
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        field: 'created_at',
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+    });
   },
 
   async down(queryInterface) {
--- db/migrations/20220827000406-order-product.js
+++ db/migrations/20220827000406-order-product.js
@@ -1,35 +1,13 @@
 'use strict';
 
-const { ORDER_PRODUCT_TABLE } = require('../models/order-product.model');
-const { CUSTOMER_TABLE } = require('../models/customer.model');
-const { DataTypes, Sequelize } = require('sequelize');
+const {
+  ORDER_PRODUCT_TABLE,
+  OrderProductSchema,
+} = require('../models/order-product.model');
 
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(ORDER_PRODUCT_TABLE, {
-      id: {
-        primaryKey: true,
-        autoIncrement: true,
-        type: DataTypes.INTEGER,
-      },
-      customerId: {
-        field: 'customer_id',
-        allowNull: false,
-        type: DataTypes.INTEGER,
-        references: {
-          model: CUSTOMER_TABLE,
-          key: 'id',
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-      },
-      createdAt: {
-        allowNull: false,
-        field: 'created_at',
-        type: DataTypes.DATE,
-        defaultValue: Sequelize.fn('NOW'),
-      },
-    });
+    await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
   },
 
   async down(queryInterface) {
