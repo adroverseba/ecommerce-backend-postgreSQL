23251d668386b6975839cd343b2e8cd61ec4d961
adroverseba <adroversebastian.sa@gmail.com>
2022-10-14 14:04:11 -0300

    fix migration order-product

--- db/migrations/20220827000406-order-product.js
+++ db/migrations/20220827000406-order-product.js
@@ -1,13 +1,35 @@
 'use strict';
 
-const {
-  ORDER_PRODUCT_TABLE,
-  OrderProductSchema,
-} = require('../models/order-product.model');
+const { ORDER_PRODUCT_TABLE } = require('../models/order-product.model');
+const { CUSTOMER_TABLE } = require('../models/customer.model');
+const { DataTypes, Sequelize } = require('sequelize');
 
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
+    await queryInterface.createTable(ORDER_PRODUCT_TABLE, {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+      },
+      customerId: {
+        field: 'customer_id',
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: CUSTOMER_TABLE,
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      createdAt: {
+        allowNull: false,
+        field: 'created_at',
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+    });
   },
 
   async down(queryInterface) {
