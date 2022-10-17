307b4a59dc04ac7a81ad4955518e784704d447d4
adroverseba <adroversebastian.sa@gmail.com>
2022-10-17 01:56:06 -0300

    update cors2

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 const whitelist = [
   'http://localhost:8080',
   'https://myapp.co',
-  'https://adroverseba.github.io/',
+  'https://adroverseba.github.io/react-super-shop-ecommerce',
 ];
 const options = {
   origin: (origin, callback) => {
