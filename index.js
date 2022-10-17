be92d8ecf4cb1c5e6a65f8745c6d74fc0ad2318b
adroverseba <adroversebastian.sa@gmail.com>
2022-10-17 01:47:49 -0300

    update cors

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const whitelist = [
+  'http://localhost:8080',
+  'https://myapp.co',
+  'https://adroverseba.github.io/',
+];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
